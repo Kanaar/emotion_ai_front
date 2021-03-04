36f4b837983042ad7dc50621895adffe4c54e8ad
Richard van den Broek <vandenbroek.rp@gmail.com>
2021-03-04 17:55:47 -0300

    taking snapshots

--- app.py
+++ app.py
@@ -2,37 +2,62 @@ import streamlit as st
 import imutils
 import cv2
 import numpy as np
+import SessionState
 
 '''
 # Emotion Ai front
 '''
-
+# session_state = SessionState.get(api_image="", snap_btn=st.button('snapshot'))
 FRAME_WINDOW = st.image([])
 FRAME_WINDOW_2 = st.image([])
 camera = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-snap_btn = st.button('snapshot')
-# while True:
-#     ret, frame = camera.read()
-#     color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#     FRAME_WINDOW.image(color)
+box = st.checkbox('box')
 
-if snap_btn:
-    st.write('SNAP!')
-    ret, frame_2 = camera.read()
-    gray = cv2.cvtColor(frame_2, cv2.COLOR_BGR2GRAY)
-    FRAME_WINDOW_2.image(gray)
-    faces = faceCascade.detectMultiScale(
-        gray,
+def get_faces_coordinates(canvas):
+    return faceCascade.detectMultiScale(
+        canvas,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(48, 48),
+        minSize=(55, 55)
     )
-    for (x, y, w, h) in faces:
-        rect_image = cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        FRAME_WINDOW_2.image(rect_image)
 
+def get_face_frames(canvas, faces_coordinates):
+    for (x, y, w, h) in faces_coordinates:
+        return canvas[y:y + h, x:x + w]
 
+def display(window, canvas, faces_coordinates, rectangles=False):
+    if rectangles:
+        for (x, y, w, h) in faces_coordinates:
+            display_canvas = cv2.rectangle(canvas, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        window.image(display_canvas)
+    else:
+        window.image(canvas)
+
+def call_api(data):
     # body = {'X': cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)}
     # response = requests.post(url='/api/path-to-your-endpoint/', files=body)
     # st.write(print(response))
+    pass
+
+if st.button('Snapshot'):
+    ret, frame = camera.read()
+    gray_canvas =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces_coordinates = get_faces_coordinates(gray_canvas)
+    image = get_face_frames(gray_canvas, faces_coordinates)
+    data = cv2.resize(image, dsize=(48, 48), interpolation=cv2.INTER_CUBIC)
+    # emotion = call_api(data)
+    st.write(data)
+
+# while True:
+#     ret, frame = camera.read()
+#     color_canvas = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     gray_canvas =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     faces_coordinates = get_faces_coordinates(gray_canvas)
+#     if box:
+#         display(FRAME_WINDOW, color_canvas, faces_coordinates, rectangles=True)
+#     else:
+#         display(FRAME_WINDOW, color_canvas, faces_coordinates)
+
+
+
