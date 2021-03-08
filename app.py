d9717f4a866f53af6067b99c29ff207cc4fb37d8
Richard van den Broek <vandenbroek.rp@gmail.com>
2021-03-08 15:33:37 -0300

    static photo upload, refactored rendering

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import app_photo
 import app_video
 import streamlit as st
 PAGES = {"Video": app_video, "Photo": app_photo}
+
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 box = st.sidebar.checkbox("box faces")
